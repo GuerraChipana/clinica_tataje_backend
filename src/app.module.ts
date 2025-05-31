5e66dc480180e5bf99d5b641d6c608ea70392870
Luis Guerra <Fernandoguerrachipana@gmail.com>
2025-05-30 23:25:38 -0500

    Inicio del Backend

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(), 
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => ({
+        type: 'mysql',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [join(__dirname, '**/*.entity{.ts,.js}')],
+        synchronize: false,
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [],
+})
+export class AppModule {}
